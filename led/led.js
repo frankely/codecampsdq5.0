0ee0cf737f7840101bda70b548c3916250f3291f
Frankely Diaz <diazfrankely@gmail.com>
2016-01-30 14:37:57 -0400

    presentacion actualizada

--- led/led.js
+++ led/led.js
@@ -4,7 +4,7 @@ var board = new five.Board();
 board.on("ready", function() {
 
   // Create a standard `led` component instance
-  var led = new five.Led(5);
+  var led = new five.Led(13);
 
   // "blink" the led in 500ms
   // on-off phase periods
